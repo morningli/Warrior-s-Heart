feffdede9c07c0c7c3ec44d6b6bbb5ecfc21365a
sunstarzixin <23152192@qq.com>
2014-10-25 15:20:42 +0800

    munepage Signed-off-by: sunstarzixin <23152192@qq.com>

---
+++ src/Assets/Scripts/Model/Menu/ProfilePage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProfilePage : BasePage {
+
+
+	static ProfilePage m_instance;
+	public static ProfilePage Instance
+	{
+		get
+		{
+			if (m_instance == null)
+			{
+				m_instance = ResourceManager.Load("Prefab/Menu/ProfilePage").GetComponent<ProfilePage>();
+			}
+			return m_instance;
+		}
+	}
+	
+	
+	void Awake()
+	{
+		UIEventListener.Get (gameObject.FindChild ("startGameButton")).onClick = OnStartGameClick;
+	}
+	
+	void OnStartGameClick(GameObject go)
+	{
+		Debug.Log ("134");
+		//Application.LoadLevel ("Game");
+	}
+}
