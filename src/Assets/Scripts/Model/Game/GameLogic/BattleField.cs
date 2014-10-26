0c55c884031a1b13a3986998fa006ea35966d6ce
kelingjie1 <kelingjie1@126.com>
2014-10-27 00:04:12 +0800

    修改资源文件，开发游戏战斗场景
    Signed-off-by: kelingjie1 <kelingjie1@126.com>

---
+++ src/Assets/Scripts/Model/Game/GameLogic/BattleField.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleField : MonoBehaviour 
+{
+    List<Worrior> m_AttackerList;
+    List<Worrior> m_DefenderList;
+
+    void Update()
+    {
+
+    }
+}
