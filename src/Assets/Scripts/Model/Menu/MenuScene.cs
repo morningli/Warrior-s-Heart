8253eb64de59b758f98db6719425cde45e3b8436
sunstarzixin <23152192@qq.com>
2014-11-08 10:55:24 +0800

    删除profile

--- src/Assets/Scripts/Model/Menu/MenuScene.cs
+++ src/Assets/Scripts/Model/Menu/MenuScene.cs
@@ -5,7 +5,7 @@ public class MenuScene : MonoBehaviour
 {
 	void Start()
 	{
-		PageManager.Instance.ShowPage(ProfilePage.Instance);
+		PageManager.Instance.ShowPage(ShopMenuPage.Instance);
 	}
 
     void Update()
