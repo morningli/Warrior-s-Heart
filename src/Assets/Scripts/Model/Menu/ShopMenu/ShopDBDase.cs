8b09765929872357c2edf89ffe0b9ae7729be1c6
sunstarzixin <23152192@qq.com>
2014-11-08 12:03:30 +0800

    sasun

--- src/Assets/Scripts/Model/Menu/ShopMenu/ShopDBDase.cs
+++ src/Assets/Scripts/Model/Menu/ShopMenu/ShopDBDase.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class ShopDBbase : MonoBehaviour {
+enum ENUM_ItemType
+{
+	Comm_Item,
+};
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+enum ENUM_ItemState
+{
+	COMM_STATE,	  //普通类型
+	SOLDED_STATE, //卖完
+};
+
+public struct ShopItem
+{	
+	public string strItemShopID;//商品id
+	public string strItemName; 
+	public int iItemType;  //定义物品类型 参考ItemType
+	public int iGoldPrice;
+	public int iDiamondPices;
+	public int iItemState; //物品状态 参考
+	public int iItemNum;
+}
+
+public class ShopDBbase  {
+
+	List<ShopItem> m_listShopItem;
+	int m_iMoney;
+	int m_GoldPrice;
 }
